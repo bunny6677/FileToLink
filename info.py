9ed28e9d7a09b63550dd299345d0c2e159d71a82
bunny6677 <165284399+bunny6677@users.noreply.github.com>
2024-03-28 19:01:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,10 +1,12 @@
 import os
 # Bot information
-API_ID = os.getenv('API_ID', "")
-API_HASH = os.getenv('API_HASH', "")
-BOT_TOKEN = os.getenv('BOT_TOKEN', "")
+API_ID = os.getenv('API_ID', "23089557")
+API_HASH = os.getenv('API_HASH', "4a98e047ba5b456892b5043107c0a61f")
+BOT_TOKEN = os.getenv('BOT_TOKEN', "7062808485:AAEbe1PUdNeS8BKOFonTdzwd3Zhg0ywdasw")
 
 # stream vars
 PORT = int(os.getenv('PORT', '5050'))
-BIN_CHANNEL = os.getenv("BIN_CHANNEL", "")
-URL = os.getenv("URL", "")
+BIN_CHANNEL = os.getenv("BIN_CHANNEL", "-1002008101396")
+URL = os.getenv("URL", "mongodb+srv://charanrajkendyala1177:Pubg1838@cluster0.k5rmybi.mongodb.net/?retryWrites=true&w=majority")
+OWNER_USERNAME = os.getenv('OWNER_USERNAME', "Vijay_malia")
+OWNER_ID = os.getenv('OWNER_ID', "5480645152")
